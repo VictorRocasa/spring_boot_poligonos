c116c174d3d9a2c75104f3a81e09362f1d45c6a0
Victor <victorcapobianco01@yahoo.com.br>
2022-09-02 13:42:16 -0300

    Classes Service e Repository criadas

---
+++ src/main/java/com/example/Poligono/PoligonoController.java
@@ -0,0 +1,22 @@
+package com.example.Poligono;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/CRUD")
+public class PoligonoController {
+    
+    private final PoligonoService poligonoService;
+
+    @Autowired
+	public PoligonoController(PoligonoService poligonoService) { 
+        this.poligonoService = poligonoService;
+    }
+
+    @GetMapping
+	public List<Poligono> getPoligonos(){
+        return poligonoService.getPoligonos();
+    }
+}
---
+++ src/main/java/com/example/Poligono/PoligonoRepository.java
@@ -0,0 +1,7 @@
+package com.example.Poligono;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PoligonoRepository extends JpaRepository<Poligono, Integer>{
+
+}
