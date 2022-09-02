b6ea1ecd701151be1e5f9a95b06adc8c373066e3
Victor <victorcapobianco01@yahoo.com.br>
2022-09-02 15:05:19 -0300

    Erro interno resolvido

--- src/main/java/com/example/demo/Poligono/PoligonoController.java
+++ src/main/java/com/example/demo/Poligono/PoligonoController.java
@@ -1,21 +1,15 @@
 package com.example.demo.Poligono;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping(path = "api/v1/poligono")
+@RequestMapping(path = "api/poligonos/CRUD")
 public class PoligonoController {
     
     private final PoligonoService poligonoService;
